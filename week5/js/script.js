abbeccb351261143f0cb8bfe8225c5628d0a6473
Thijs Cornelissen <thijs_cornelissen@hotmail.com>
2016-08-09 23:55:23 +0200

    upload week5

--- week5/js/script.js
+++ week5/js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -246,7 +246,7 @@ function buildAndShowMenuItemsHTML (categoryMenuItems) {
 function buildMenuItemsViewHtml(categoryMenuItems,
                                 menuItemsTitleHtml,
                                 menuItemHtml) {
-
+  console.log(categoryMenuItems);
   menuItemsTitleHtml =
     insertProperty(menuItemsTitleHtml,
                    "name",
